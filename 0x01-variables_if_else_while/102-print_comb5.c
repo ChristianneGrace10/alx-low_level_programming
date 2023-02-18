b48220e10873a29187169c08e23597a5434ce522
christianne luhata <christianneluhata@gmail.com>
2023-02-17 19:16:41 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,10 +11,9 @@ int main(void)
 
 {
 	int p, q;
-
+        
 	for (p = 0; p <= 98; p++)
-	{	
-	
+        {
 	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
@@ -26,7 +25,7 @@ int main(void)
 	continue;
 	putchar(',');
 	putchar(' ');
-	}
+        }
 	}
 	putchar('\n');
 	return (0);
