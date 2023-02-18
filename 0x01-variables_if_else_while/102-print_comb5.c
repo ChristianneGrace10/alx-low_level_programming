071eda9734f8e23adfef981f909f8aee5fadcf12
christianne luhata <christianneluhata@gmail.com>
2023-02-17 20:23:25 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,10 +10,10 @@ int main(void)
 {	int c = 0;
 	int f_d;
 	int l_d;
-	
 	int c2;
 	int f_d2;
 	int l_d2;
+
 	while (c <= 98)
 	{
 		f_d = (c / 10 + '0');
@@ -22,7 +22,8 @@ int main(void)
 		while (c2 <= 99)
 		{
 			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');                        if (c < c2)
+			l_d2 = (c2 % 10 + '0');
+			if (c < c2)
 			{
 				putchar(f_d);
 				putchar(l_d);
