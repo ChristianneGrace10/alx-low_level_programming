d5a32befbd74dfa0c9c75f248a97b4a3e7e1fe84
christianne luhata <christianneluhata@gmail.com>
2023-02-17 20:26:30 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,19 @@
 
 int main(void)
 {
-	int digit2, digit3, digit4;
+	int digit1, digit2, digit3;
 
-	for (digit2 = 0; digit2 < 8; digit2++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (digit3 = digit2 + 2; digit3 < 9; digit3++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (digit4 = digit3 + 1; digit4 < 10; digit4++)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
+				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
-				putchar((digit4 % 10) + '0');
 
-				if (digit2 == 7 && digit3 == 8 && digit4 == 9)
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
