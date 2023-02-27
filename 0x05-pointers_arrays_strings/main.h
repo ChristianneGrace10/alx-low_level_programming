85be1f391980110b98de6bcc7a469f2b6f57d8f7
christianne luhata <christianneluhata@gmail.com>
2023-02-27 12:38:58 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -12,8 +15,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
 
 #endif
-
